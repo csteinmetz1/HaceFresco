23cdef79240941484eee89f4e349f9eeeb36d3b6
csteinmetz1 <csteinmetz1@gmail.com>
2017-12-06 21:56:31 -0500

    crude random word search

--- hacefresco.py
+++ hacefresco.py
@@ -1,10 +1,26 @@
 import spotipy
-import pprint
 from spotipy.oauth2 import SpotifyClientCredentials
-import os
+import pprint
+import random
+import json
+
+# choose random search term for wordlist
+def randomWord():
+    with open('english-words/words_dictionary.json') as wordlist:
+        w = json.load(wordlist)
+        q = random.choice(list(w.keys()))
+
+    return q
 
 client_credentials_manager = SpotifyClientCredentials()
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
-results = sp.search("sondorblue", limit=1)
-pprint.pprint(results['tracks']['items'][0]['album'])
+result = sp.search(randomWord(), limit=1, type='track')
+searches = 1
+
+while result['tracks']['items'] == []:
+    result = sp.search(randomWord(), limit=1, type='track')
+    searches += 1
+
+print(searches)
+print(result)
