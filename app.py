294c7d73c05d4ad172dbb7dbc68f0742c6cafc8c
csteinmetz1 <csteinmetz1@gmail.com>
2017-12-12 10:37:25 -0500

    updated structure and cleaned up a little bit. css font not working

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask, render_template
  
 app = Flask(__name__)
  
-@app.route('/<string:page_name>/')
-def render_static(page_name):
-    return render_template('%s.html' % page_name)
+@app.route('/')
+def index():
+    return render_template('index.html')
  
 if __name__ == '__main__':
     app.run()
